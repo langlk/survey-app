65d906b21b6d555560a1089d25eaa83513816649
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-27 15:28:23 -0700

    Add validation to Answer to prevent blanks or invalid questions.

--- lib/answer.rb
+++ lib/answer.rb
@@ -2,4 +2,5 @@
 
 class Answer < ActiveRecord::Base
   belongs_to(:question)
+  validates(:answer_text, :question, presence: true)
 end
--- spec/answer_spec.rb
+++ spec/answer_spec.rb
@@ -5,6 +5,20 @@ require "spec_helper"
 describe('Answer') do
   let(:survey) { Survey.create({title: "Best Pizza in Seattle"}) }
 
+  describe '#save' do
+    it "does not allow a blank answer" do
+      question = Question.create({question_text: "What is your favorite kind of Pizza?", number: 1, survey_id: survey.id})
+      answer = Answer.new({answer_text: "", question_id: question.id})
+      expect(answer.save).to eq(false)
+    end
+
+    it "does not allow an answer to be assigned to a question that doesn't exist" do
+      question = Question.create({question_text: "What is your favorite kind of Pizza?", number: 1, survey_id: survey.id})
+      answer = Answer.create({answer_text: "New York", question_id: (question.id + 1)})
+      expect(answer.save).to eq(false)
+    end
+  end
+
   describe '#question' do
     it "returns the question that an answer belongs to" do
       question = Question.create({question_text: "What is your favorite kind of Pizza?", number: 1, survey_id: survey.id})
