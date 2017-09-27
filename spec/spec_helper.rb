f060b73f2db44cfdd93364d98cc0be9fa2906fd4
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-27 15:06:38 -0700

    Add tests for question UI.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,15 @@ set(:show_exceptions, false)
 require('./app')
 
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Survey.all.each do |survey|
+      survey.destroy
+    end
+
+    Question.all.each do |question|
+      question.destroy
+    end
+  end
+end
--- spec/survey_integration_spec.rb
+++ spec/survey_integration_spec.rb
@@ -1,13 +1,6 @@
 require "spec_helper"
 
 describe 'The survey design path', {type: :feature} do
-  it "allows user to add a survey" do
-    visit('/design/surveys')
-    fill_in('title', with: 'Favorite Programming Languages')
-    click_button('Add Survey')
-    expect(page).to have_content('Favorite Programming Languages')
-  end
-
   it "does not allow blank titles" do
     visit('/design/surveys')
     fill_in('title', with: '')
@@ -15,7 +8,7 @@ describe 'The survey design path', {type: :feature} do
     expect(page).to have_content("Title can't be blank")
   end
 
-  it "allows user to edit surveys" do
+  it "allows user to add and edit surveys" do
     visit('/design/surveys')
     fill_in('title', with: 'Favorite Programming Languages')
     click_button('Add Survey')
@@ -33,5 +26,32 @@ describe 'The survey design path', {type: :feature} do
     click_link('Edit')
     click_button('Delete Survey')
     expect(page).to have_no_content('Best Pizza Places')
+  end
+
+  it "allows user to create and edit questions for a survey" do
+    visit('/design/surveys')
+    fill_in('title', with: 'Best Pizza Places')
+    click_button('Add Survey')
+    fill_in('number', with: '1')
+    fill_in('question-text', with: "What is your favorite kind of Pizza?")
+    click_button('Add Question')
+    expect(page).to have_content('What is your favorite kind of Pizza?')
+    click_link('Edit or Remove')
+    fill_in('question-text', with: "What is your favorite style of Pizza?")
+    click_button('Update Question')
+    expect(page).to have_content("What is your favorite style of Pizza?")
+    expect(page).to have_no_content("What is your favorite kind of Pizza?")
+  end
+
+  it "allows user to delete questions" do
+    visit('/design/surveys')
+    fill_in('title', with: 'Best Pizza Places')
+    click_button('Add Survey')
+    fill_in('number', with: '1')
+    fill_in('question-text', with: "What is your favorite kind of Pizza?")
+    click_button('Add Question')
+    click_link('Edit or Remove')
+    click_button('Delete Question')
+    expect(page).to have_no_content('What is your favorite kind of Pizza?')
   end
 end
