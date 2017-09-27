6113d157303915fa985299b806417a396ad1d318
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-27 13:50:38 -0700

    Add UI for adding and viewing surveys.

--- app.rb
+++ app.rb
@@ -2,3 +2,30 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get '/' do
+  erb(:index)
+end
+
+get '/surveys/:mode' do
+  @mode = params[:mode]
+  @surveys = Survey.all
+  erb(:surveys)
+end
+
+get '/surveys/:mode/:id' do
+  @mode = params[:mode]
+  @survey = Survey.find(params[:id].to_i)
+  erb(:survey)
+end
+
+post '/surveys/design' do
+  @survey = Survey.new({title: params["title"]})
+  if @survey.save
+    redirect "/surveys/design/#{@survey.id}"
+  else
+    @surveys = Survey.all
+    @mode = "design"
+    erb(:surveys)
+  end
+end
