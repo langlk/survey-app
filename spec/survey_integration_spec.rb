09e3e9070522b9bd992b541543c069e3c6071271
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-27 14:22:32 -0700

    Add tests for survey design UI.

--- spec/survey_integration_spec.rb
+++ spec/survey_integration_spec.rb
@@ -1,1 +1,37 @@
 require "spec_helper"
+
+describe 'The survey design path', {type: :feature} do
+  it "allows user to add a survey" do
+    visit('/design/surveys')
+    fill_in('title', with: 'Favorite Programming Languages')
+    click_button('Add Survey')
+    expect(page).to have_content('Favorite Programming Languages')
+  end
+
+  it "does not allow blank titles" do
+    visit('/design/surveys')
+    fill_in('title', with: '')
+    click_button('Add Survey')
+    expect(page).to have_content("Title can't be blank")
+  end
+
+  it "allows user to edit surveys" do
+    visit('/design/surveys')
+    fill_in('title', with: 'Favorite Programming Languages')
+    click_button('Add Survey')
+    click_link('Edit')
+    fill_in('title', with: 'Favourite Programming Languages')
+    click_button('Update Survey')
+    expect(page).to have_content('Favourite Programming Languages')
+    expect(page).to have_no_content('Favorite Programming Languages')
+  end
+
+  it "allows user to delete surveys" do
+    visit('/design/surveys')
+    fill_in('title', with: 'Best Pizza Places')
+    click_button('Add Survey')
+    click_link('Edit')
+    click_button('Delete Survey')
+    expect(page).to have_no_content('Best Pizza Places')
+  end
+end
